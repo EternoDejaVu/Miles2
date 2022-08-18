2f7b8bc14241b89f018cc9f51925057de98c64f7
EternoDejaVu <105560004+EternoDejaVu@users.noreply.github.com>
2022-08-18 12:53:26 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2000;
+        int miles = cost / 20;
+        System.out.println("Миль за покупку:" + miles);
+    }
+}
